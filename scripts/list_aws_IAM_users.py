8203f0424ead9c0db3a762a8540b3f28c5ae6afb
Ananthi S <135607480+ananthi-selvam@users.noreply.github.com>
2025-05-02 09:03:45 +0530

    Update list_aws_IAM_users.py
    pushing into inside the folder

--- scripts/list_aws_IAM_users.py
+++ scripts/list_aws_IAM_users.py
@@ -1,1 +1,14 @@
+#importing boto3 modules
+import boto3
+from pprint import pprint
 
+#opening AWS mangement console
+aws_management_console = boto3.session.Session(profile_name='default')
+iam_console = aws_management_console.client('iam')
+
+result = iam_console.list_users() 
+#pprint(result['Users'])
+
+#iterating users
+for user in result['Users']:
+    print(user['UserName'])
